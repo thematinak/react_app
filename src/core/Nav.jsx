b37347f205a2b6390b3306a039545e157ffa786b
Martin Pecen <thematinak@gmail.com>
2023-04-28 12:56:37 +0200

    Nav fix

--- src/core/Nav.jsx
+++ src/core/Nav.jsx
@@ -5,13 +5,13 @@ export default function NavBar() {
     const pathName = useLocation().pathname
     return <Nav variant="tabs" defaultActiveKey={pathName}>
         <Nav.Item>
-            <NavLink className='nav-link' to="/" eventKey='/'>Home</NavLink>
+            <NavLink className='nav-link' to="/">Home</NavLink>
         </Nav.Item>
         <Nav.Item>
-            <NavLink className='nav-link'  to="/image" eventKey='/image'>Image</NavLink>
+            <NavLink className='nav-link'  to="/image">Image</NavLink>
         </Nav.Item>
         <Nav.Item>
-            <NavLink className='nav-link' to="/notification" eventKey='/notification'>Notification</NavLink>
+            <NavLink className='nav-link' to="/notification">Notification</NavLink>
         </Nav.Item>
     </Nav>
     
